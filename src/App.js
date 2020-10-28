a96249da79f76f3d4debca553de7971d24473d3e
LauraC-art <u1201955@unimilitar.edu.co>
2020-10-28 13:15:42 -0500

    Parallax

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import { ParticlesScreen } from './ParticlesInt'
 import ReactPlayer from 'react-player'
+import "./App.scss";
 
 // Little helpers ...
+const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
 class App extends React.Component {
   render () {
     return (
-    	<div style={{
-        position: "relative",
-        paddingTop: "56.25%" }}>
-        <ReactPlayer
-          url='https://vimeo.com/243556536'
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%"}}
-          playing
-          width='100%'
-          height='100%'
-        />
-      </div>
+      <Parallax ref={ref => (this.parallax = ref)} pages={3}>
+        
+        <div>
+          <div style={{
+            position: "relative",
+            paddingTop: "56.25%" }}>
+            <ReactPlayer
+              url='https://www.youtube.com/watch?v=UaTcNdy8v_g'
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%"}}
+              playing
+              width='100%'
+              height='100%'
+            />
+          </div>
+        </div>
+        <ParallaxLayer offset={0} speed={1} style={{ backgroundColor: '#805E73' }}/> 
+        
+        <ParallaxLayer offset={0.3} speed={0.3} factor={3}>
+          <img className="Container__images" src="https://brandemia.org/sites/default/files/sites/default/files/grupo_imagen_logo.jpg"/>
+          <div className="Container__images">
+            <div className="Typos__titles">
+              <h1> Convertirse en ingeniero en multimedia </h1>
+            </div>
+          </div>
+        </ParallaxLayer> 
+        
+        <ParallaxLayer offset={0} speed={0} factor={3}
+        style={{ backgroundImage: url('stars', true),
+        backgroundSize: 'cover' }} />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.2}
+          style={{ position: 'relative' }}>
+            <ParticlesScreen/>
+        </ParallaxLayer>
+
+      </Parallax>
     )
   }
 }
