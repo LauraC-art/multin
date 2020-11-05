4ed0d33faad91b22cd8ce1f4ba64080db02a3f44
LauraC-art <u1201955@unimilitar.edu.co>
2020-11-05 13:12:48 -0500

    tests_rendimiento

--- src/App.js
+++ src/App.js
@@ -2,35 +2,63 @@ import React from 'react'
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import {ParticlesScreen} from './ParticlesInt'
 import ReactPlayer from 'react-player'
+import Overlay from 'react-image-overlay'
 import "./App.scss"
 
 // Little helpers ...
 /*asdasd*/
 const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
-const a=["https://www.itconsultors.com/images/blog/Realidad-virtual-con-gafas.jpg",""];
+const a=["https://www.itconsultors.com/images/blog/Realidad-virtual-con-gafas.jpg","https://upload.wikimedia.org/wikipedia/commons/e/ef/UMNG_Campus_Nueva_Granada_-_Acceso_Principal.jpg"];
 class App extends React.Component {
   render () {
     return (
       <Parallax ref={ref => (this.parallax = ref)} pages={3}>
-        <div>
-        <ParallaxLayer offset={0.4} speed={0.1} style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}>
-            <div className="Container__images">            
-                <img className="img-fluid"
-                src={a[0]}/>
-                <div className="container Container__text">
-                  <h1 className="Typos__titles" style={{zIndex:"5"}}>
-                        Convertirse en <b>ingeniero en multimedia</b>
-                  </h1>
-                </div>
-              </div>                           
-            </ParallaxLayer>
+
+        {/*Página 2: Scroll horizontal*/}
+      <div>
+        <ParallaxLayer offset={0.5} speed={-0.5}
+        factor={1}
+        style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}>
+          
+          <div className="Container__images">      
+            <img className="img-fluid"
+            src={a[0]}
+            style={{opacity:"0.7"}}/>
+            <div className="container Container__text">
+              <h1 className="Typos__titles" style={{zIndex:"5"}}>
+                    Convertirse en <b>ingeniero en multimedia</b>
+              </h1>
             </div>
+          </div>                           
+        </ParallaxLayer>
+        </div>
+
+        {/*Página 3: Scroll horizontal*/}
+        <div>
+        <ParallaxLayer offset={2} speed={1}
+        factor={3}
+        style={{ backgroundColor: 'rgba(0,0,0,0.4)' }}>
+
+        <div>
+            <div style={{}}>
+
+              <ReactPlayer
+                url='https://www.youtube.com/watch?v=3VdYIIATMu8'
+                style={{
+                  position: "absolute"}}
+                width='50%'
+              />
+            </div>
+          </div>
+                                 
+        </ParallaxLayer>
+        </div>
         
+        {/*Página 1: Vídeo*/}
         <ParallaxLayer
           offset={0}
-          speed={2}>
-            
-        
+          speed={2}
+          factor={1}>              
           <div>
             <div style={{
               position: "relative",
@@ -51,13 +79,13 @@ class App extends React.Component {
             </div>
           </div>
         </ParallaxLayer>        
-        
 
         <ParallaxLayer
           offset={0}
           speed={0.2}
-          style={{ position: 'absolute', zIndex:'-5' }}>            
-            {/*}<ParticlesScreen/>*/}
+          style={{ position: 'absolute', zIndex:'-5' }}
+          factor={1}>            
+              {/*<ParticlesScreen/>*/}
         </ParallaxLayer>
 
       </Parallax>
